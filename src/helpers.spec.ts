cd5de82f882309e18b79259001a14c8348061e95
Andrey Edounov <opty@opty.ru>
2022-05-06 22:02:12 +0300

    feat: added unit tests

--- src/helpers.spec.ts
+++ src/helpers.spec.ts
@@ -1,5 +1,70 @@
+import { data } from './data';
 import { getCompanyFilters, getCountryFilters, getDataSource } from './helpers';
 
 describe(nameof(getCompanyFilters), () => {
-  it('', () => {});
+  it('returns filters', () => {
+    expect(getCompanyFilters(data)).toEqual([
+      {
+        text: 'ToTheMoon',
+        value: 'ToTheMoon'
+      },
+      {
+        text: 'MyCoin',
+        value: 'MyCoin'
+      },
+      {
+        text: 'OverTheHills',
+        value: 'OverTheHills'
+      },
+      {
+        text: 'InTheDark',
+        value: 'InTheDark'
+      },
+      {
+        text: 'BetterFuture',
+        value: 'BetterFuture'
+      }
+    ]);
+  });
+
+  it('returns empty array', () => {
+    expect(getCompanyFilters([])).toEqual([]);
+  });
+});
+
+describe(nameof(getCountryFilters), () => {
+  it('returns filters', () => {
+    expect(getCountryFilters(data)).toEqual([
+      {
+        text: 'SG',
+        value: 'SG'
+      },
+      {
+        text: 'UK',
+        value: 'UK'
+      },
+      {
+        text: 'US',
+        value: 'US'
+      }
+    ]);
+  });
+});
+
+it('returns empty array', () => {
+  expect(getCountryFilters([])).toEqual([]);
+});
+
+describe(nameof(getDataSource), () => {
+  it('returns data with «key»', () => {
+    const result = getDataSource(data);
+
+    for (const item of result) {
+      expect(item.key).toBe(item.id);
+    }
+  });
+
+  it('returns empty array', () => {
+    expect(getDataSource([])).toEqual([]);
+  });
 });
