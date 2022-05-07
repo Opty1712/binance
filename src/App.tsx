06f92c73769bb53d6a80aede4e57c0564b19b521
Andrey Edounov <opty@opty.ru>
2022-05-07 12:30:02 +0300

    feat: added recharts example

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,19 @@ import {
   PageHeader,
   Typography
 } from 'antd';
-import { data, Data } from './data';
 import 'antd/dist/antd.css';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer
+} from 'recharts';
+
+import { data, Data } from './data';
 import {
   getCompanyFilters,
   getCountryFilters,
@@ -57,6 +68,31 @@ export const App = memo(() => {
       />
       <br />
       <br />
+
+      <LineChart
+        width={500}
+        height={300}
+        data={data2}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="pv"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      </LineChart>
     </>
   );
 });
@@ -140,5 +176,50 @@ const columns: TableColumnProps<Data>[] = [
     sorter: (a, b) => a.coin_price - b.coin_price,
     render: renderFinance,
     align: 'right'
+  }
+];
+
+const data2 = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100
   }
 ];
